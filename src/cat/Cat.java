27ad04dca808ac352d3e1541948e14bf7b0f8690
kimzehong <nevergive5p@naver.com>
2024-08-29 11:45:24 +0900

    7장 객체지향 1, 2

---
+++ src/cat/Cat.java
@@ -0,0 +1,41 @@
+package cat;
+
+public class Cat {
+    private String name;
+
+    private String breeds;
+
+    double weight;
+
+    void claw(){
+        System.out.println("할퀴기");
+    }
+
+    void meow(){
+        System.out.println("야옹 !!!");
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public String getBreeds() {
+        return breeds;
+    }
+
+    public void setBreeds(String breeds) {
+        this.breeds = breeds;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/cat/Main.java
@@ -0,0 +1,26 @@
+package cat;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        Cat cat = new Cat();
+        cat.setName("Aicat");
+        cat.setBreeds("Persian");
+        cat.setWeight(3.156783);
+
+        System.out.printf("고양이 이름: %s\n",cat.getName());
+
+        System.out.printf("고양이 품종: %s\n",cat.getBreeds());
+
+        System.out.printf("고양이 몸무게: %.2fkg\n",cat.getWeight());
+
+        System.out.println(cat.getName()+"이 ");
+        cat.claw();
+
+        System.out.println(cat.getName()+"이 ");
+        cat.meow();
+
+    }
+}
